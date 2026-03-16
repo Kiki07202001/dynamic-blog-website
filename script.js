77a698199769b4d57f52b85a4f7414b4a4b8135d
Kiki Brusey <kikibrusey@Kikis-MacBook-Air.local>
2026-03-15 21:35:50 -0400

    Add new post creation functionality

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// To display posts
 function displayPost() {
     let post = localStorage.getItem('post')
 
@@ -20,10 +21,54 @@ posts.forEach(function(post) {
     title.textContent = post.title;
     const content = document.createElement('p')
     content.textContent = post.content;
+    const img = document.createAElement("img");
+    img.src = post.image;
+    img.width = 200;
 
     postsDiv.appendChild(title);
     postsDiv.appendChild(content);
+    postsDiv.appendChild(img)
 
     document.getElementById('container').appendChild(postsDiv);
 }
 );
+
+// To save post to Local Storage
+
+document.addEventListener("DOMContentLoaded", function() {
+
+const form = document.getElementById("postForm");
+
+form.addEventListener("submit", function(event) {
+
+event.preventDefault();
+
+const title = document.getElementById("title").value;
+const content = document.getElementById("content").value;
+const image = document.getElementById("image").value;
+
+
+let posts = localStorage.getItem("posts");
+
+if (posts) {
+    posts = JSON.parse(posts);
+    } else {
+    posts = [];
+}
+
+const newPost = {
+    id: Date.now(),
+    title: title,
+    content: content,
+    image: image
+};
+
+posts.push(newPost);
+
+localStorage.setItem("posts", JSON.stringify(posts));
+
+window.location.href = "index.html";
+
+});
+
+});
