1880b4d1b912c1a00e2838f46bc18f76852a2a6a
Kiki Brusey <kikibrusey@Kikis-MacBook-Air.local>
2026-03-16 20:05:42 -0400

    Added basic css styling and fixed JavaScript to allow styling

--- script.js
+++ script.js
@@ -13,27 +13,33 @@ function displayPost() {
 
     posts.forEach(function(post, index) {
         const postsDiv = document.createElement('div');
+        postsDiv.classList.add('post');
         const title = document.createElement('h2');
         title.textContent = post.title;
+        title.classList.add('post.title');
+
         const content = document.createElement('p');
         content.textContent = post.content;
+        content.classList.add('post-content');
 
         postsDiv.appendChild(title);
         postsDiv.appendChild(content);
 
         if (post.image) {
             const img = document.createElement('img');
+            img.classList.add('post-image');
             img.src = post.image;
             img.width = 200;
             postsDiv.appendChild(img);
         }
 
          const deleteBtn = document.createElement('button');
-            deleteBtn.textContent = 'Delete Post';
+            deleteBtn.textContent = 'Delete';
+            deleteBtn.classList.add('delete-Btn');
             deleteBtn.addEventListener('click', function() {
-            posts.splice(index, 1); // remove this post
+            posts.splice(index, 1);
             localStorage.setItem('posts', JSON.stringify(posts));
-            displayPost(); // refresh the list
+            displayPost();
         });
 
         postsDiv.appendChild(deleteBtn);
